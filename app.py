2cdb622e45f1191c1bb836b8eca66da78ee1a3a7
Jhill-Cabos <123426500+Jhill-Cabos@users.noreply.github.com>
2025-05-18 11:59:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,8 +75,13 @@ if file is not None:
             label = classify_recklessness(class_ids)
 
             for result in results[0].boxes:
-                cv2.putText(annotated_frame, label, (int(result.xyxy[0]), int(result.xyxy[1])),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                xyxy = result.xyxy[0].numpy()  # Extracting the bounding box coordinates
+                xmin, ymin, xmax, ymax = map(int, xyxy)
+                font_scale = 1.5 if label == "Reckless Driving" else 0.7
+                color = (0, 0, 255) if label == "Reckless Driving" else (0, 255, 0)
+
+                # Position the text on the frame
+                cv2.putText(annotated_frame, label, (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, 2)
 
             out.write(annotated_frame)
             stframe.image(annotated_frame, channels="BGR", use_container_width=True)
