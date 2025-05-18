9745efe14b1149265b90d7c87bf26908f31de14c
Jhill-Cabos <123426500+Jhill-Cabos@users.noreply.github.com>
2025-05-18 21:11:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 import tempfile
 import os
 
-model = YOLO("yolo4.pt")
+model = YOLO("yolo5.pt")
 
 st.set_page_config(page_title="Reckless Driving Behaviours using YOLOV11", layout="wide")
 st.title("🚗 Reckless Driving Behaviours using YOLOV11")
